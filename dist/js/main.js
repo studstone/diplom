1265a3a20f3f7a345186584698543a811fd6ce21
studstone <nikolas2996@gmail.com>
2022-06-06 15:28:44 +0300

    create modal-call

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log("first")})();
+(()=>{"use strict";(()=>{const e=document.getElementById("header").querySelector('a[href="#callback"]'),l=document.querySelector(".header-modal"),t=l.querySelector(".header-modal__close"),c=document.querySelector(".overlay");e.addEventListener("click",(()=>{c.style.cssText="\n        display: block;\n        ",l.classList.remove("header-modal")})),t.addEventListener("click",(()=>{c.style.cssText="\n        display: '';\n        ",l.classList.add("header-modal")}))})()})();
